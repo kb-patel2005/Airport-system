d915b8e19c8535fea0b8dd190e3a443a2a970593
kb-patel2005 <krishpatel1181@gmail.com>
2026-01-10 16:03:04 +0530

    optimization 1.4

--- src/assets/Components/Navbar.jsx
+++ src/assets/Components/Navbar.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect, useContext} from 'react';
+import React ,{ useState, useEffect, useContext , Suspense} from 'react';
 import { Link } from 'react-router-dom';
-import { VscMenu } from 'react-icons/vsc';
-import { GoX } from 'react-icons/go';
-import { AiOutlineUser } from "react-icons/ai";
+const VscMenu = React.lazy(() => import('react-icons/vsc').then(module => ({ default: module.VscMenu })));
+const GoX = React.lazy(() => import('react-icons/go').then(module => ({ default: module.GoX })));
+const AiOutlineUser = React.lazy(() => import('react-icons/ai').then(module => ({ default: module.AiOutlineUser })));
 import { usercontext } from '../Context/usercontext';
 import { staffcontext } from '../Context/staffcontext';
 
@@ -29,6 +29,7 @@ export default function Navbar() {
   }, [menuOpen]);
 
   return (
+    <Suspense fallback={<></>}>
     <div className='flex justify-between items-center p-5 w-full relative'
       style={{ background: 'linear-gradient(180deg, #99cbe6, transparent)' }}>
       {/* Logo */}
@@ -49,6 +50,7 @@ export default function Navbar() {
 
       {/* Menu Icon */}
       <div className='sm:hidden font-extrabold'>
+        
         <VscMenu
           className='cursor-pointer '
           style={{ fontSize: '25px', color: 'black' , fontWeight: 'bold'}}
@@ -135,5 +137,6 @@ export default function Navbar() {
         </ul>
       </div>
     </div>
+    </Suspense>
   );
 }
