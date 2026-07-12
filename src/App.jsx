4e9e7150f343ac1d7d80e0015e66d642cdec4b00
kb-patel2005 <krishpatel1181@gmail.com>
2026-07-12 11:11:20 +0530

    hemlet add in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,14 @@ function App() {
 
   return (
     <>
+      <Helmet>
+        <title>flight System management website</title>
+        <meta
+          name="description"
+          content="Airport System — book flights, manage tickets, and track flights in real time.with give seat map and flight status tracking."
+        />
+        <meta name="keywords" content="flight booking, flight ticket, airport system, live flight tracking" />
+      </Helmet>
       <staffcontext.Provider value={{ staff, setstaff }}>
         <usercontext.Provider value={{ passenger, setPassenger }}>
           <Navbar />
